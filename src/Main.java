413f744cb92706f5e7d2e5cae39d4fe38d21b7fa
Kimtai <kymdennis3@gmail.com>
2023-01-18 05:14:03 +0300

    Passing a string to our hello method

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        //method - a block of code that is executed whenever it is called upon
 
-       hello();
-       hello();
-       hello();
+        String name = "Kimtai";
+       hello(name);
+
     }
-    static void hello(){
-        System.out.println("Hello");
+    static void hello(String name){
+        System.out.println("Hello "+name);
     }
 }
