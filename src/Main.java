a33baad67718b2e998894f9d13eb0ca26c9dab2d
Kimtai <kymdennis3@gmail.com>
2023-01-18 05:28:33 +0300

    One way to code return types

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ public class Main {
     public static void main(String[] args) {
         //method - a block of code that is executed whenever it is called upon
 
-        String name = "Kimtai";
-        int age = 26;
-       hello(name, age);
+        int x = 5;
+        int y = 8;
 
+        int z = add(x,y);
+        System.out.println(z);
     }
-    static void hello(String name, int age){
-        System.out.println("Hello "+name);
-        System.out.println("You are "+age+" years old");
+    static int add(int x, int y){
+        int z = x+y;
+        return z;
     }
 }
